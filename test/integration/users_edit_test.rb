b33dee908175c53a189418a42520dfef9925a899
EC2 Default User <ec2-user@ip-172-31-16-254.us-east-2.compute.internal>
2018-10-23 07:09:24 +0000

    10.3

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,7 +6,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+=begin
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name:  "",
@@ -18,6 +20,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
  test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name  = "Foo Bar"
@@ -32,4 +35,26 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name,  @user.name
     assert_equal email, @user.email
   end
+=end
+
+=begin
+test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
+  
+=end
+
 end
